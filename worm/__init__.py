44a36e1f836cfe4463d33c67d1671f42d35d5ada
Krish Lalwani <krishlalwani846@gmail.com>
2024-06-03 13:46:54 +0530

    Create __init__.py

---
+++ worm/__init__.py
@@ -0,0 +1,11 @@
+from worm.core.console import Console
+
+def cli() -> None:
+    """ Worm Framework command-line interface.
+
+    :return None: None
+    """
+
+    console = Console()
+    console.shell()
+  
